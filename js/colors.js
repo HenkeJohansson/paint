19931f949c6b96580278a461306718f212dfe7cb
Henrik Johansson <henrik89johansson@gmail.com>
2015-11-14 22:59:51 +0100

    cleaning and small fixes

--- js/colors.js
+++ js/colors.js
@@ -9,7 +9,7 @@ function RGBToColor(r,g,b) {
 	return '#' + byteToHex(r) + byteToHex(g) + byteToHex(b);
 }
 
-var frequency = 0.3,
+var frequency = 0.10,
 	amplitude = 127,
 	center = 128,
 	width = 127,
@@ -38,8 +38,8 @@ function makeColorGradient(frequency1, frequency2, frequency3,
 }
 
 // makeColorGradient(0.3,0.3,0.3,0,2,4);
-makeColorGradient(0.10,0.10,0.10,0,2,4, 128, 127);
-makeColorGradient(frequency,frequency,frequency,0,2,4, center, width, 50);
+// makeColorGradient(0.10,0.10,0.10,0,2,4, 128, 127);
+makeColorGradient(frequency,frequency,frequency,0,2,4, center, width, 100);
 
 
 var swatches = document.getElementsByClassName('swatch');
--- js/main.js
+++ js/main.js
@@ -1,58 +1,27 @@
 
+var w = window,
+	d = document,
+	e = d.documentElement,
+	g = d.getElementsByTagName('body')[0],
+	x = w.innerWidth || e.clientWidth || g.clientWidth,
+	y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-var canvas = document.getElementById('canvas'),
-	context = canvas.getContext('2d'),
-	headerHeight = document.getElementById('header').height,
-	toolbarWidth = document.getElementById('toolbar').width,
-	radius = 10,
-	dragging = false;
 
-canvas.width = window.innerWidth - 250;
-canvas.height = window.innerHeight - 100;
-
-context.lineWidth = radius*2;
-
-var drawDot = function(e) {
-	if ( dragging ) {
-		context.lineTo( (e.clientX - 250), (e.clientY - 100) );
-		context.stroke();
-		context.beginPath();
-		context.arc( (e.clientX - 250), (e.clientY - 100) , radius, 0, Math.PI*2);
-		context.fill();
-		context.beginPath();
-		context.moveTo( (e.clientX - 250), (e.clientY - 100) );
-	}
-};
-
-var engage = function(e) {
-	dragging = true;
-	drawDot(e);
-};
-
-var disengage = function() {
-	dragging = false;
-	context.beginPath();
-	var history = context.getImageData(0,0, canvas.width, canvas.height);
-};
-
-function clearCanvas(canvas) {
-	canvas.width = window.innerWidth - 250;
-	canvas.height = window.innerHeight - 100;
+function windowResize() {
+	//
+	setColorsHeight();
 }
+window.addEventListener('resize', windowResize);
 
-canvas.addEventListener('mousedown', engage);
-canvas.addEventListener('mousemove', drawDot);
-canvas.addEventListener('mouseup', disengage);
 
-var sizeCanvas = function() {
-	var image = context.getImageData(0,0, canvas.width, canvas.height);
-	canvas.width = window.innerWidth - 250;
-	canvas.height = window.innerHeight - 100;
-	context.putImageData(image, 0, 0);
-};
 
-if (window.attachEvenet) {
-	window.attachEvent( 'onresize', sizeCanvas() );
-} else if (window.addEventListener) {
-	window.addEventListener( 'resize', sizeCanvas );
+function setColorsHeight() {
+	var upperCont = document.getElementById('upper-content').clientHeight,
+		colors = document.getElementById('colors'),
+		wh = w.innerHeight;
+
+	var remainingHeight = wh - (upperCont + 20);
+
+	colors.setAttribute('style', 'height:' + remainingHeight + 'px;' );
 }
+setColorsHeight();
